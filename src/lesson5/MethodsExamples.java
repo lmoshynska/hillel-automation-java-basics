1071aedfc5d9a37f94659536060d7800ba7ec734
Liubov Moshynska <lmoshynska@protonmail.com>
2021-09-01 22:23:58 +0300

    Added solution to task 1 from classwork

--- src/lesson5/MethodsExamples.java
+++ src/lesson5/MethodsExamples.java
@@ -3,11 +3,26 @@ package lesson5;
 public class MethodsExamples {
 
     public static void main(String[] args) {
-        int first = 2;
-        int second = 3;
+//        int first = 2;
+//        int second = 3;
+//
+//        int max = findMax(first, second);
+//        System.out.println("Max value between numbers is: " + max);
+        int num = 6;
+        boolean result = isEven(num);
+        System.out.println(result);
+    }
 
-        int max = findMax(first, second);
-        System.out.println("Max value between numbers is: " + max);
+    public static boolean isEven(int number) {
+//        if(number % 2 == 0) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+
+//        return (number % 2 == 0) ? true : false;
+
+        return number % 2 == 0;
     }
 
     public static int findMax(int a, int b) {

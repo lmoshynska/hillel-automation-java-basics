869af835036ddd1efcfcc5aea18f14d7a9d65ca9
Liubov Moshynska <lmoshynska@protonmail.com>
2021-10-11 21:42:07 +0300

    Added materials for lesson 14

---
+++ src/lesson14/CollectionsStorage.java
@@ -0,0 +1,51 @@
+package lesson14;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class CollectionsStorage {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static List<String> getStrArrayList() {
+        List<String> list = new ArrayList<>();
+        list.add("Mango");
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Grapes");
+
+        return list;
+    }
+
+    public static List<Integer> getIntegerArrayList() {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        return list;
+    }
+
+    public static List<String> getLinkedList() {
+        List<String> list = new LinkedList<String>();
+        list.add("Kyiv");
+        list.add("London");
+        list.add("Rome");
+        list.add("Paris");
+        list.add("Paris");
+        list.add("Paris");
+
+        return list;
+    }
+
+    public static Set<String> getHashSet() {
+        Set<String> list = new HashSet<String>();
+        list.add("Kyiv");
+        list.add("London");
+        list.add("Rome");
+        list.add("Paris");
+
+        return list;
+    }
+}
